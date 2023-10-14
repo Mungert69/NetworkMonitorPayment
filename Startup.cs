0944c81f6ba69377e3d4f3b21d8b2756cf6be2ef
Mungert69 <contact@mahadeva.co.uk>
2023-10-14 05:21:43 +0200

    fixed packages

--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;   
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
