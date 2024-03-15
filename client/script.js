9dea59c15dfb9374b97f52056faec6971cadd07a
Mungert69 <contact@mahadeva.co.uk>
2024-03-15 21:46:42 +0100

    removed stripe client folder

--- client/script.js
+++
@@ -1,9 +0,0 @@
-/* Fetch prices and update the form */
-fetch("http://localhost:2058/config")
-  .then(r => r.json())
-  .then(({basicPrice, proPrice}) => {
-    const basicPriceInput = document.querySelector('#basicPrice');
-    basicPriceInput.value = basicPrice;
-    const proPriceInput = document.querySelector('#proPrice');
-    proPriceInput.value = proPrice;
-  })
